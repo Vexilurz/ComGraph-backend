f4eb662a47f56bf37a371ebea7c5c8163dcfc5a6
Sergei Potekhin <vexilurz@gmail.com>
2023-11-14 14:23:56 +0300

    add some route functions

--- src/controllers/PortController.js
+++ src/controllers/PortController.js
@@ -6,6 +6,27 @@ class PortController {
       res.status(500).json(e)
     }
   }
+
+  async getByName(req, res) {
+    try {
+      const {name} = req.params
+      res.json(`okok: ${name}`)
+    } catch (e) {
+      res.status(500).json(e)
+    }
+  }
+
+  async connect(req, res) {
+    try {
+      const {name} = req.body
+      if (name !== undefined)
+        res.json(`connect: ${name}`)
+      else
+        throw "no name"
+    } catch (e) {
+      res.status(500).json(e)
+    }
+  }
 }
 
 export default new PortController()
--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import PortController from "./controllers/PortController.js";
 const router = new Router()
 
 router.get('/ports', PortController.getExisting)
-router.get('/port/:name')
-router.post('/connect/:name')
+router.get('/port/:name', PortController.getByName)
+router.post('/connect', PortController.connect)
 
 export default router
