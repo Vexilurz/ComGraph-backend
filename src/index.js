a19153f99fb7b44c32c797fe804432f1f9c16b53
Sergei Potekhin <vexilurz@gmail.com>
2023-11-14 13:45:08 +0300

    test get and post requests

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const PORT = 11148;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
+  console.log(req.query);
   res.status(200).json('ComGraph server working')
+})
+app.post('/post', (req, res) => {
+  console.log(req.body);
+  res.status(200).json('ok')
 })
 
 app.listen(PORT, () => console.log(`ComGraph server started on port ${PORT}`))
