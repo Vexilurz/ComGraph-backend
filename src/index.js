902e4acb15a43d94a709bbd1a9d492f9f4a511c4
Sergei Potekhin <vexilurz@gmail.com>
2023-11-14 13:34:53 +0300

    run server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('test')
+import express from 'express'
+
+const PORT = 11148;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json('ComGraph server working')
+})
+
+app.listen(PORT, () => console.log(`ComGraph server started on port ${PORT}`))
